c383fd7ab6edd923975caac37e4a29770d63f69e
SahilKumarSingh01 <sahil.kumar.singh.lol9@gmail.com>
2025-10-04 07:56:06 +0530

    more debug statements added

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ import { WebSocketServer, WebSocket } from 'ws';
 
 const PORT = process.env.PORT || 8080;
 
+function debugPrintToken(token) {
+  console.log("[DEBUG] raw:", token);
+
+  let hexView = "";
+  for (let i = 0; i < token.length; i++) {
+    const ch = token.charCodeAt(i);
+    hexView += ch.toString(16).padStart(2, "0") + " ";
+  }
+  console.log("[DEBUG] Token hex:", hexView.trim());
+}
 const wss = new WebSocketServer({ port: PORT });
 console.log(`[Proxy] WebSocket server running on port ${PORT}`);
 
@@ -19,8 +29,10 @@ wss.on('connection', (clientWs, req) => {
     console.log('[Proxy] Missing Authorization header');
     return;
   }
-  console.log(`[Proxy] trying to connect upstream: ${upstreamUrl} ✅`);
-  console.log(`[Proxy] trying with authheader : ${authHeader} ✅`);
+  // console.log(`[Proxy] trying to connect upstream: ${upstreamUrl} ✅`);
+  // console.log(`[Proxy] trying with authheader : ${authHeader} ✅`);
+  debugPrintToken(upstreamUrl);
+  debugPrintToken(authHeader);
   const upstream = new WebSocket(upstreamUrl, { headers: { Authorization: authHeader } });
 
   upstream.on('open', () => {
